17a0ba954666e1026adac651b5325bacdbfdb5f1
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2024-04-15 17:37:27 +0200

    SDP2324VC2-247 Aanmaken mockdata

---
+++ src/data/mock-data.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        GEBRUIKERID:1,
+        EMAILADRES:"michel@outlook.be",
+        WACHTWOORD:"1234",
+        NAAM:"Michel",
+        ROL:"KLANT",
+        ISACTIEF:1
+    },
+    {
+        GEBRUIKERID:2,
+        EMAILADRES:"christophe@outlook.com",
+        WACHTWOORD:"1234",
+        NAAM:"Christophe",
+        ROL:"LEVERANCIER",
+        ISACTIEF:1
+    },
+    {
+        GEBRUIKERID:74,
+        EMAILADRES:"joris@outlook.com",
+        WACHTWOORD:"1234",
+        NAAM:"Joris",
+        ROL:"ADMINISTRATOR",
+        ISACTIEF:1
+    },
+]
