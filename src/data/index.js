874e0aea815731deeb3999fd1279eb71a01db7df
Jasper Vandenbroucke <jasper.vandenbroucke@student.hogent.be>
2024-04-24 10:23:28 +0200

    SDP2324VC2-252 extra klant en leverancier info bij getAll en getById

--- src/data/index.js
+++ src/data/index.js
@@ -1,25 +1,25 @@
-const { join } = require('path');
+const { join } = require("path");
 
-const config = require('config');
-const knex = require('knex');
+const config = require("config");
+const knex = require("knex");
 
-const { getLogger } = require('../core/logging');
+const { getLogger } = require("../core/logging");
 
-const NODE_ENV = config.get('env');
-const isDevelopment = NODE_ENV === 'development';
+const NODE_ENV = config.get("env");
+const isDevelopment = NODE_ENV === "development";
 
-const DATABASE_CLIENT = config.get('database.client');
-const DATABASE_NAME = config.get('database.name');
-const DATABASE_HOST = config.get('database.host');
-const DATABASE_PORT = config.get('database.port');
-const DATABASE_USERNAME = config.get('database.username');
-const DATABASE_PASSWORD = config.get('database.password');
+const DATABASE_CLIENT = config.get("database.client");
+const DATABASE_NAME = config.get("database.name");
+const DATABASE_HOST = config.get("database.host");
+const DATABASE_PORT = config.get("database.port");
+const DATABASE_USERNAME = config.get("database.username");
+const DATABASE_PASSWORD = config.get("database.password");
 
 let knexInstance;
 
 async function initializeData() {
   const logger = getLogger();
-  logger.info('Initializing connection to the database');
+  logger.info("Initializing connection to the database");
 
   const knexOptions = {
     client: DATABASE_CLIENT,
@@ -45,7 +45,7 @@ async function initializeData() {
 
   // Check the connection, create the database and then reconnect
   try {
-    await knexInstance.raw('SELECT 1+1 AS result');
+    await knexInstance.raw("SELECT 1+1 AS result");
     await knexInstance.raw(`CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME}`);
 
     // We need to update the Knex configuration and reconnect to use the created database by default
@@ -54,35 +54,35 @@ async function initializeData() {
 
     knexOptions.connection.database = DATABASE_NAME;
     knexInstance = knex(knexOptions);
-    await knexInstance.raw('SELECT 1+1 AS result');
+    await knexInstance.raw("SELECT 1+1 AS result");
   } catch (error) {
     logger.error(error.message, { error });
-    throw new Error('Could not initialize the data layer');
+    throw new Error("Could not initialize the data layer");
   }
 
-//   // Run migrations
-//   try {
-//     await knexInstance.migrate.latest();
-//   } catch (error) {
-//     logger.error('Error while migrating the database', {
-//       error,
-//     });
+  //   // Run migrations
+  //   try {
+  //     await knexInstance.migrate.latest();
+  //   } catch (error) {
+  //     logger.error('Error while migrating the database', {
+  //       error,
+  //     });
 
-//     // No point in starting the server when migrations failed
-//     throw new Error('Migrations failed, check the logs');
-//   }
+  //     // No point in starting the server when migrations failed
+  //     throw new Error('Migrations failed, check the logs');
+  //   }
 
-//   if (isDevelopment) {
-//     try {
-//       await knexInstance.seed.run();
-//     } catch (error) {
-//       logger.error('Error while seeding database', {
-//         error,
-//       });
-//     }
-//   }
+  //   if (isDevelopment) {
+  //     try {
+  //       await knexInstance.seed.run();
+  //     } catch (error) {
+  //       logger.error('Error while seeding database', {
+  //         error,
+  //       });
+  //     }
+  //   }
 
-  logger.info('Succesfully connected to the database');
+  logger.info("Succesfully connected to the database");
 
   return knexInstance;
 }
@@ -90,7 +90,7 @@ async function initializeData() {
 function getKnex() {
   if (!knexInstance)
     throw new Error(
-      'Please initialize the data layer before getting the Knex instance',
+      "Please initialize the data layer before getting the Knex instance"
     );
   return knexInstance;
 }
@@ -98,18 +98,20 @@ function getKnex() {
 async function shutdownData() {
   const logger = getLogger();
 
-  logger.info('Shutting down database connection');
+  logger.info("Shutting down database connection");
 
   await knexInstance.destroy();
   knexInstance = null;
 
-  logger.info('Database connection closed');
+  logger.info("Database connection closed");
 }
 
 const tables = Object.freeze({
-  gebruiker: 'GEBRUIKER',
-  product: 'PRODUCT',
-  bestelling: 'BESTELLING',
+  gebruiker: "GEBRUIKER",
+  product: "PRODUCT",
+  bestelling: "BESTELLING",
+  klant: "KLANT",
+  leverancier: "LEVERANCIER",
 });
 
 module.exports = {
--- src/repository/bestelling.js
+++ src/repository/bestelling.js
@@ -1,32 +1,126 @@
 const { getLogger } = require("../core/logging");
 const { tables, getKnex } = require("../data");
 
-const getAll = async (gebruikerId) => {
-  const bestellingen = getKnex()(tables.bestelling)
-    .select()
-    .where("KLANT_GEBRUIKERID", gebruikerId)
-    .orWhere("LEVERANCIER_GEBRUIKERID", gebruikerId)
-    .orderBy("ORDERID", "DESC");
+const SELECT_COLUMS = [
+  `${tables.bestelling}.ORDERID`,
+  `${tables.bestelling}.BETALINGSTATUS`,
+  `${tables.bestelling}.BETALINGSDATUM`,
+  `${tables.bestelling}.DATUMGEPLAATST`,
+  `${tables.bestelling}.HERINNERINGSDATUM`,
+  `${tables.bestelling}.ORDERSTATUS`,
+  `${tables.bestelling}.KLANT_GEBRUIKERID`,
+  `${tables.klant}.TELEFOONNUMMER as KLANT_TELEFOONNUMMER`,
+  `u1.EMAILADRES as KLANT_EMAILADRES`,
+  `${tables.klant}.LAND as KLANT_LAND`,
+  `${tables.klant}.POSTCODE as KLANT_POSTCODE`,
+  `${tables.klant}.STAD as KLANT_STAD`,
+  `${tables.klant}.STRAAT as KLANT_STRAAT`,
+  `${tables.klant}.STRAATNR as KLANT_STRAATNR`,
+  `${tables.klant}.BEDRIJF_NAAM as KLANT_BEDRIJF_NAAM`,
+  `${tables.bestelling}.LEVERANCIER_GEBRUIKERID`,
+  `${tables.leverancier}.BEDRIJF_NAAM as LEVERANCIER_BEDRIJF_NAAM`,
+  `u2.EMAILADRES as LEVERANCIER_EMAILADRES`,
+];
 
-  return bestellingen;
+const formatBestelling = ({
+  KLANT_GEBRUIKERID,
+  KLANT_TELEFOONNUMMER,
+  KLANT_EMAILADRES,
+  KLANT_LAND,
+  KLANT_POSTCODE,
+  KLANT_STAD,
+  KLANT_STRAAT,
+  KLANT_STRAATNR,
+  KLANT_BEDRIJF_NAAM,
+  LEVERANCIER_GEBRUIKERID,
+  LEVERANCIER_BEDRIJF_NAAM,
+  LEVERANCIER_EMAILADRES,
+  ...rest
+}) => ({
+  ...rest,
+  klant: {
+    GEBRUIKERID: KLANT_GEBRUIKERID,
+    TELEFOONNUMMER: KLANT_TELEFOONNUMMER,
+    EMAILADRES: KLANT_EMAILADRES,
+    LAND: KLANT_LAND,
+    POSTCODE: KLANT_POSTCODE,
+    STAD: KLANT_STAD,
+    STRAAT: KLANT_STRAAT,
+    STRAATNR: KLANT_STRAATNR,
+    BEDRIJF_NAAM: KLANT_BEDRIJF_NAAM,
+  },
+  leverancier: {
+    GEBRUIKERID: LEVERANCIER_GEBRUIKERID,
+    EMAILADRES: LEVERANCIER_EMAILADRES,
+    BEDRIJF_NAAM: LEVERANCIER_BEDRIJF_NAAM,
+  },
+});
+
+const getAll = async (gebruikerId) => {
+  const bestellingen = await getKnex()(tables.bestelling)
+    .join(
+      tables.klant,
+      `${tables.bestelling}.KLANT_GEBRUIKERID`,
+      "=",
+      `${tables.klant}.GEBRUIKERID`
+    )
+    .join(
+      { u1: tables.gebruiker },
+      `${tables.klant}.GEBRUIKERID`,
+      "=",
+      `u1.GEBRUIKERID`
+    )
+    .join(
+      tables.leverancier,
+      `${tables.bestelling}.LEVERANCIER_GEBRUIKERID`,
+      "=",
+      `${tables.leverancier}.GEBRUIKERID`
+    )
+    .join(
+      { u2: tables.gebruiker },
+      `${tables.leverancier}.GEBRUIKERID`,
+      "=",
+      `u2.GEBRUIKERID`
+    )
+    .where(`${tables.bestelling}.KLANT_GEBRUIKERID`, gebruikerId)
+    .orWhere(`${tables.bestelling}.LEVERANCIER_GEBRUIKERID`, gebruikerId)
+    .select(SELECT_COLUMS)
+    .orderBy(`${tables.bestelling}.ORDERID`, "DESC");
+
+  return bestellingen.map(formatBestelling);
 };
 
-const getById = (id, gebruikerId) => {
-  const bestelling = getKnex()(tables.bestelling)
+const getById = async (id, gebruikerId) => {
+  const bestelling = await getKnex()(tables.bestelling)
+    .join(
+      tables.klant,
+      `${tables.bestelling}.KLANT_GEBRUIKERID`,
+      "=",
+      `${tables.klant}.GEBRUIKERID`
+    )
+    .join(
+      { u1: tables.gebruiker },
+      `${tables.klant}.GEBRUIKERID`,
+      "=",
+      `u1.GEBRUIKERID`
+    )
+    .join(
+      tables.leverancier,
+      `${tables.bestelling}.LEVERANCIER_GEBRUIKERID`,
+      "=",
+      `${tables.leverancier}.GEBRUIKERID`
+    )
+    .join(
+      { u2: tables.gebruiker },
+      `${tables.leverancier}.GEBRUIKERID`,
+      "=",
+      `u2.GEBRUIKERID`
+    )
     .where("KLANT_GEBRUIKERID", gebruikerId)
     .andWhere("ORDERID", id)
     .orWhere("LEVERANCIER_GEBRUIKERID", gebruikerId)
     .andWhere("ORDERID", id)
-    .first();
-  // const bestelling = getKnex()(tables.bestelling)
-  //   .where("ORDERID", id)
-  //   .andWhere(function () {
-  //     this.where("KLANT_GEBRUIKERID", gebruikerId).orWhere(
-  //       "LEVERANCIER_GEBRUIKERID",
-  //       gebruikerId
-  //     );
-  //   })
-  //   .first();
+    .first(SELECT_COLUMS);
 
   return bestelling;
 };
