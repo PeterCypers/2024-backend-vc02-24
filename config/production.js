4ec93778f1c6cfdd0f1ad0a3efc7a292786fe921
Tiemen Deroose <tiemen.deroose@student.hogent.be>
2024-05-22 18:53:15 +0200

    Production configuratie toevoegen voor online zetten

--- config/production.js
+++ config/production.js
@@ -8,4 +8,16 @@ module.exports = {
     origins: ["http://localhost:5173"], // het adres waar je front-end op draait
     maxAge: 3 * 60 * 60,
   },
+  database: {
+    client: "mysql2",
+    name: "SDP2_2324_DB_VC02",
+  },
+  auth: {
+    jwt: {
+      secret: "hq5eE8qvt1v89qc46r5ze4ntw6iKry31yhDsv2h13gh1e4vqg9Ar4g1vBrq",
+      expirationInterval: 60 * 60 * 1000, // ms (1 hour)
+      issuer: "b2bportal.delaware.be",
+      audience: "b2bportal.delaware.be",
+    },
+  },
 };
